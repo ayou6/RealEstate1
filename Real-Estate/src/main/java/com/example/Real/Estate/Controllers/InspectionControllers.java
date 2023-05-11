ed37d611e4ca0a30ad9b2825f997e2ba47fdb26d
ayou6 <ayoxb.abbas@gmail.com>
2023-05-11 15:27:10 +0400

    started with Inspection

--- Real-Estate/src/main/java/com/example/Real/Estate/Controllers/InspectionControllers.java
+++ Real-Estate/src/main/java/com/example/Real/Estate/Controllers/InspectionControllers.java
@@ -1,4 +1,23 @@
 package com.example.Real.Estate.Controllers;
 
+import com.example.Real.Estate.Model.Inspection;
+import com.example.Real.Estate.Services.InspectionServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+@RestController
+@RequestMapping(value = "inspection")
 public class InspectionControllers {
+    @Autowired
+    InspectionServices inspectionServices;
+
+    @RequestMapping(value = "getAll", method = RequestMethod.GET)
+    List<Inspection> getAllInspections(){
+        return inspectionServices.getAllInspections();
+        // http://localhost:8080/Inspection/getAll
+
+    }
+
 }
--- Real-Estate/src/main/java/com/example/Real/Estate/Repository/InspectionRepository.java
+++ Real-Estate/src/main/java/com/example/Real/Estate/Repository/InspectionRepository.java
@@ -1,4 +1,7 @@
 package com.example.Real.Estate.Repository;
 
-public interface InspectionRepository {
+import com.example.Real.Estate.Model.Inspection;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InspectionRepository extends JpaRepository<Inspection,Long> {
 }

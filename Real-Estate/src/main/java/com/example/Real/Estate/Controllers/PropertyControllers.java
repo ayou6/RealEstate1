6a34760b59e1e3bbc9fe6e19498e36ca808a3050
ayou6 <ayoxb.abbas@gmail.com>
2023-05-11 16:23:58 +0400

    started property class

--- Real-Estate/src/main/java/com/example/Real/Estate/Controllers/PropertyControllers.java
+++ Real-Estate/src/main/java/com/example/Real/Estate/Controllers/PropertyControllers.java
@@ -1,4 +1,23 @@
 package com.example.Real.Estate.Controllers;
 
-public class PropertyControllers {
+import com.example.Real.Estate.Model.Property;
+import com.example.Real.Estate.Services.PropertyServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequestMapping(value = "Property")
+@RestController
+    public class PropertyControllers {
+        @Autowired
+        PropertyServices propertyServices;
+
+        @RequestMapping(value = "getAll", method = RequestMethod.GET)
+        public List<Property> getAllProperties(){
+            return propertyServices.getAllProperties();
+
+        }
 }

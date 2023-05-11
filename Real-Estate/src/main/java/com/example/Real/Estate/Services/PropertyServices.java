7bdaa54982c1c47222bb59e0376a91d8b142705d
ayou6 <ayoxb.abbas@gmail.com>
2023-05-11 16:24:36 +0400

    property class Service

--- Real-Estate/src/main/java/com/example/Real/Estate/Services/PropertyServices.java
+++ Real-Estate/src/main/java/com/example/Real/Estate/Services/PropertyServices.java
@@ -1,4 +1,16 @@
 package com.example.Real.Estate.Services;
 
+import com.example.Real.Estate.Model.Property;
+import com.example.Real.Estate.Repository.PropertyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 public class PropertyServices {
+    @Autowired
+    PropertyRepository propertyRepository;
+
+    public List<Property> getAllProperties(){
+        return propertyRepository.findAll();
+    }
 }

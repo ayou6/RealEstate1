23c03ff44d3c17899cd364811be1f66ffa423ad1
ayou6 <ayoxb.abbas@gmail.com>
2023-05-11 16:50:45 +0400

    some classes editted

--- Real-Estate/src/main/java/com/example/Real/Estate/Controllers/ContractControllers.java
+++ Real-Estate/src/main/java/com/example/Real/Estate/Controllers/ContractControllers.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
+@RestController
+@RequestMapping(value = "contract")
 public class ContractControllers {
     @Autowired
     ContractServices contractServices;

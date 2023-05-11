c5231b5ddc6d9cbe6e795ed9c3789eedd589e95f
ayou6 <ayoxb.abbas@gmail.com>
2023-05-11 16:27:58 +0400

    seller service  completed

--- Real-Estate/src/main/java/com/example/Real/Estate/Services/SellerServices.java
+++ Real-Estate/src/main/java/com/example/Real/Estate/Services/SellerServices.java
@@ -1,4 +1,19 @@
 package com.example.Real.Estate.Services;
 
+import com.example.Real.Estate.Model.Seller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
 public class SellerServices {
+
+    @Autowired
+    SellerServices sellerServices;
+
+    @RequestMapping(value = "getAll",method = RequestMethod.GET)
+    public List<Seller> getAllSellers(){
+        return sellerServices.getAllSellers();
+    }
 }

3e4acc7e7e13356d39bb1fa90e31021ace021394
ayou6 <ayoxb.abbas@gmail.com>
2023-05-11 16:27:45 +0400

    seller controller completed

--- Real-Estate/src/main/java/com/example/Real/Estate/Controllers/SellerControllers.java
+++ Real-Estate/src/main/java/com/example/Real/Estate/Controllers/SellerControllers.java
@@ -1,4 +1,20 @@
 package com.example.Real.Estate.Controllers;
+import com.example.Real.Estate.Model.Seller;
+import com.example.Real.Estate.Services.SellerServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+@RestController
+@RequestMapping(value = "Seller")
+    public class SellerControllers {
 
-public class SellerControllers {
+        @Autowired
+        SellerServices sellerServices;
+
+        @RequestMapping(value = "getAll",method = RequestMethod.GET)
+        public List<Seller> getAllSellers(){
+            return sellerServices.getAllSellers();
+
+        }
+
 }
